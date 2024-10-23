8848d90bc8226237c3a22466edd2a912a8e0c59f
ardevd <ardevd@users.noreply.github.com>
2024-10-23 19:55:52 +0200

    fix: removed unused token variable

--- ha_dimo/dimo_client.py
+++ ha_dimo/dimo_client.py
@@ -9,7 +9,7 @@ class DimoClient:
         self.dimo = auth.get_dimo()
 
     async def init(self):
-        token = await self.auth.get_token()
+        await self.auth.get_token()
 
     async def get_vehicle_makes(self):
         return await self.dimo.device_definitions.list_device_makes()
