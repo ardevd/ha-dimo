278dd5aefcae5ddf9f6c41bca6d59eb5604d07c3
ardevd <ardevd@protonmail.com>
2024-11-11 14:49:19 +0100

    refactor: put dimo api code in separate directory

---
+++ custom_components/dimo/dimoapi/auth.py
@@ -0,0 +1,31 @@
+import asyncio
+import dimo as dimo_api
+from loguru import logger
+
+
+class Auth:
+    def __init__(self, client_id, domain, private_key, dimo=None):
+        self.client_id = client_id
+        self.domain = domain
+        self.private_key = private_key
+        self.token = None
+        self.dimo = dimo if dimo else dimo_api.DIMO("Production")
+        
+
+    async def _get_auth(self):
+        logger.info("Retrieving access token")
+        auth_header = await self.dimo.auth.get_token(
+            client_id=self.client_id,
+            domain=self.domain,
+            private_key=self.private_key,
+        )
+        self.token = auth_header["access_token"]
+        logger.debug(f"access token: {self.token}")
+        
+    async def get_token(self):
+        if self.token is None:
+            await self._get_auth()
+        return self.token
+
+    def get_dimo(self):
+        return self.dimo
---
+++ custom_components/dimo/dimoapi/dimo_client.py
@@ -0,0 +1,14 @@
+import asyncio
+from loguru import logger
+from auth import Auth
+
+class DimoClient:
+    def __init__(self, auth: Auth):
+        self.auth = auth
+        self.dimo = auth.get_dimo()
+
+    async def init(self):
+        await self.auth.get_token()
+
+    async def get_vehicle_makes(self):
+        return await self.dimo.device_definitions.list_device_makes()
