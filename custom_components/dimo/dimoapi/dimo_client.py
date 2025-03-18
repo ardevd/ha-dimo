b51d7490b04c04ad74822a40617d4d599cc3b112
ardevd <edvard.holst@gmail.com>
2025-03-18 22:52:08 +0100

    refactor: improved exception handling for DIMO vehicle count retrieval

--- custom_components/dimo/dimoapi/dimo_client.py
+++ custom_components/dimo/dimoapi/dimo_client.py
@@ -106,9 +106,12 @@ class DimoClient:
         try:
             result = self.dimo.identity.count_dimo_vehicles()
             return result.get("data", {}).get("vehicles", {}).get("totalCount")
+        except ConnectionError as ex:
+            _LOGGER.warn("DIMO API request error when retrieving DIMO vehicle count %s", ex)
+            return None
         except Exception as e:
             _LOGGER.error(f"Failed to get total DIMO vehicles: {e}")
-            return None
+            raise
 
     def _check_sacd_permissions(self, token_id) -> Optional[VehicleSharingPermission]:
         """
