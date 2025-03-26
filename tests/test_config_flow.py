300adea65140fbd7df1c219a92605c50a6fb3c13
ardevd <edvard.holst@gmail.com>
2025-03-26 22:09:22 +0100

    added more tests

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -1,4 +1,11 @@
-from custom_components.dimo.const import DOMAIN
+from custom_components.dimo.const import (
+    DOMAIN,
+    CONF_LICENSE_ID,
+    CONF_AUTH_PROVIDER,
+    CONF_PRIVATE_KEY,
+)
+
+from custom_components.dimo.config_flow import InvalidAuth
 import pytest
 
 
@@ -15,3 +22,52 @@ async def test_flow_user_step_no_input(hass):
     )
     assert result["type"] == "form"
     # At this stage, no user input was provided so it just shows the form
+
+
+async def test_flow_invalid_auth(hass, monkeypatch):
+    """Test the flow when invalid credentials are provided."""
+
+    async def mock_validate_input(hass, data):
+        raise InvalidAuth
+
+    monkeypatch.setattr(
+        "custom_components.dimo.config_flow.validate_input", mock_validate_input
+    )
+
+    user_input = {
+        CONF_LICENSE_ID: "dummy_client",
+        CONF_AUTH_PROVIDER: "dummy_provider",
+        CONF_PRIVATE_KEY: "dummy_private",
+    }
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "user"}, data=user_input
+    )
+
+    assert result["type"] == "form"
+    assert result["errors"]["base"] == "invalid_auth"
+
+
+async def test_flow_valid_input(hass, monkeypatch):
+    """Test a successful config flow."""
+
+    async def mock_validate_input(hass, data):
+        return {"title": "DIMO"}
+
+    monkeypatch.setattr(
+        "custom_components.dimo.config_flow.validate_input", mock_validate_input
+    )
+
+    user_input = {
+        CONF_LICENSE_ID: "dummy_client",
+        CONF_AUTH_PROVIDER: "dummy_provider",
+        CONF_PRIVATE_KEY: "dummy_private",
+    }
+
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": "user"}, data=user_input
+    )
+
+    assert result["type"] == "create_entry"
+    assert result["title"] == "DIMO"
+    assert result["data"] == user_input
